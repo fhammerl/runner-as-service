b03c4cb99e888f1e21300a6fcd0eaea448f27923
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2021-06-07 12:26:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo $RUNNER_CFG_PAT
 [ -z "$RUNNER_CFG_PAT" ] && echo "RUNNER_CFG_PAT is required" && exit 1 || true
 [ -z "$RUNNER_REPOSITORY" ] && echo "RUNNER_REPOSITORY is required" && exit 1 || true
 curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/create-latest-svc.sh | bash -s $RUNNER_REPOSITORY
